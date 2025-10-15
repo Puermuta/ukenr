2d7cdb6f92afa93f4c6f842dd774dc29b14299e1
Puermuta <19885813+Puermuta@users.noreply.github.com>
2025-10-15 21:29:13 +0200

    Split languages to separate files.

---
+++ main.js
@@ -0,0 +1,12 @@
+function getWeekNumber(date) {
+      const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+      const dayNum = d.getUTCDay() || 7;
+      d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+      const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+      return Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
+    }
+
+const weekNum = getWeekNumber(new Date());
+const weekString = `Week ${weekNum}`
+document.getElementById("week-container").textContent = weekString;
+document.title = weekString;
